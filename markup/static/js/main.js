a2819906f40a97930a828aca1b63d673d8b049cd
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2018-05-09 19:31:39 +0300

    upload

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+$(document).ready(function(){
+  $(".slider").owlCarousel({
+    loop: true,
+    items: 1,
+    slideTransition: 'ease',
+    smartSpeed: 350
+  });
+});
+
 /*
     This file can be used as entry point for webpack!
  */
