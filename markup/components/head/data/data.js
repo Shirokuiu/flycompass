047a66f05f05bd2b15947b02106bc22e2cd79f85
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2018-05-11 15:45:54 +0300

    upload services

--- markup/components/head/data/data.js
+++ markup/components/head/data/data.js
@@ -1,6 +1,26 @@
 head: {
-    defaults: {
-        title: 'default title',
+    index: {
+        title: 'Home',
         useSocialMetaTags: true
+    },
+    services: {
+      title: 'Services',
+      useSocialMetaTags: true
+    },
+    about: {
+      title: 'About',
+      useSocialMetaTags: true
+    },
+    contact: {
+      title: 'Contact us',
+      useSocialMetaTags: true
+    },
+    blog: {
+      title: 'Blog',
+      useSocialMetaTags: true
+    },
+    learnMore: {
+      title: 'Learn more',
+      useSocialMetaTags: true
     }
 }
